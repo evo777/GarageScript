5f6b734eb5a7c947130e2ce2d62039a1f11057d5
Emmanuel Vo <Emmanuel@Emmanuel-Vos-MacBookPro.local>
2017-08-15 13:09:03 -0700

    Completed project

--- node-readdir/names.js
+++ node-readdir/names.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+const getNames = (err, file) => {
+  const result = [];
+  file.forEach(name => {
+    if (name !== 'emmanuel') {
+      result.push(name);
+    }
+  });
+  fs.writeFile('/Users/Emmanuel/Desktop/names.txt', result.join('\r\n'));
+};
+
+fs.readdir('/Users/Emmanuel/Desktop/names', getNames);
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
