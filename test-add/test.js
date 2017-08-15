4120f33e1806f9093749ffe0d048db7ead66d0d6
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-08-14 19:08:17 -0700

    Moved console.log inside function

--- test-add/test.js
+++ test-add/test.js
@@ -2,13 +2,13 @@ const add = require('./add.js');
 
 const test = (a, b, c) => {
   if (add(a, b) === c) {
-    return 'Correct';
+    console.log('Correct');
   }
-  return 'Incorrect';
+  console.log('Incorrect');
 };
 
-console.log(test(1, 2, 3));
-console.log(test(8, 10, 4));
-console.log(test(3, 4, 7));
-console.log(test(9, 2, 5));
-console.log(test(4,11,20));
+test(1, 2, 3);
+test(8, 10, 4);
+test(3, 4, 7);
+test(9, 2, 5);
+test(4,11,20);
