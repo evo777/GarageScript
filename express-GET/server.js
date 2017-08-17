6ff31dbc740f5ac1a5c420d9d67d4ae42584037c
Emmanuel Vo <Emmanuel@Emmanuel-Vos-MacBookPro.local>
2017-08-17 13:52:10 -0700

    Rendered names that is not mine

--- express-GET/server.js
+++ express-GET/server.js
@@ -4,10 +4,18 @@ const fs = require('fs');
 const port = 3000;
 const app = express();
 
-app.use(express.static('public'));
 
 app.get('/llip', (req, res) => {
-  fs.appendFile('./public/message.txt', req.query.field1+': '+req.query.field2+'\r\n');
+  const getName = (err, file) => {
+    const result = [];
+    file.forEach(name => {
+      if (name !== 'emmanuel') {
+        result.push(name);
+      }
+    });
+    res.send(result+'\r\n');
+  };
+  fs.readdir('/Users/Emmanuel/Desktop/home', getName);
 });
 
 app.listen(port, () => {
