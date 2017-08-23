568e5afd63f054690cec604ae5231c6effbe70e1
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-08-22 19:46:28 -0700

    Completed lesson 7

---
+++ lesson7-GetPost/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.post('/llip', (req, res) => {
+  fs.appendFile('./public/message.txt', req.body.name+': '+req.body.comment+'\r\n');
+});
+
+app.listen(port, () => {
+  console.log('Listening on port 3000');
+});
