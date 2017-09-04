2d458347b5daf992283861f040750889b6ebae51
Emmanuel <emmanuel@llip.life>
2017-09-04 14:09:02 -0700

    server

--- lesson9-meme/server.js
+++ lesson9-meme/server.js
@@ -4,18 +4,14 @@ const bodyParser = require('body-parser');
 const gm = require('gm');
 
 const app = express();
-const port = 3000;
+const port = 3150;
 
 app.use(express.static('public'));
 app.use(bodyParser.json({limit: '500mb'}));
 
 app.post('/picture', (req, res) => {
-  fs.appendFile(`/Users/evo/Desktop/llipio/GarageScript/lesson9-meme/public/${req.body.name}.png`, `${req.body.name}: ${req.body.comment}`);
-});
-
-app.post('/picture', (req, res) => {
-  let imagePath = `/Users/evo/Desktop/llipio/GarageScript/lesson9-meme/public/${req.body.name}.png`;
-  let meme = `/Users/evo/Desktop/llipio/GarageScript/lesson9-meme/public/meme/${req.body.name}.png`
+  let imagePath = `/home/emmanuel/GarageScript/lesson9-meme/public/${req.body.name}.png`;
+  let meme = `/home/emmanuel/GarageScript/lesson9-meme/public/meme/${req.body.name}.png`
   fs.writeFile(imagePath, req.body.image.replace('data:image/png;base64',''),'base64', () => {
     gm(imagePath).fontSize(50).drawText(50,50, req.body.comment).write(meme, (err) => {
       console.log('Error: ', err);
